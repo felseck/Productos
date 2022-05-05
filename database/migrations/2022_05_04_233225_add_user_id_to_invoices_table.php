3c3978b956e70c4c01bb71442976f7efe8216ed8
Felipe Solorzano <felseck@hotmail.com>
2022-05-04 19:15:51 -0500

    invoices

--- database/migrations/2022_05_04_233225_add_user_id_to_invoices_table.php
+++ database/migrations/2022_05_04_233225_add_user_id_to_invoices_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::table('invoices', function (Blueprint $table) {
             $table->integer('user_id')->nullable();
+            $table->integer('user_id')->nullable();
         });
     }
 
